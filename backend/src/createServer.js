ee3e499a86c851495d5292be9d33dfbf61056f25
Josh <joshwestbury@gmail.com>
2019-02-28 21:48:45 -0600

    add createServer, db.js and schema.graphql files

---
+++ backend/src/createServer.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require("graphql-yoga");
+const Mutation = require("./resolvers/Mutation");
+const Query = require("./resolver/Query");
+const db = require("./db");
+
+//create graphQL Yoga server
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: "src/schema.graphql",
+    resolvers: { Mutation, Query },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
+}
+
+module.export = createServer;
