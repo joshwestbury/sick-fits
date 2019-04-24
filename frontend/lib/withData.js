71aca63b5c1c10118f9f99e532c216f03a6fc960
Josh <joshwestbury@gmail.com>
2019-04-23 22:05:37 -0500

    set up Apollo Client with React

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,18 +1,20 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import withApollo from "next-with-apollo";
+import ApolloClient from "apollo-boost";
+import { endpoint } from "../config";
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    connectToDevTools: true,
+    cache,
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          credentials: 'include',
+          credentials: "include"
         },
-        headers,
+        headers
       });
-    },
+    }
   });
 }
 
